2d75b6a964c4d40f8a190fa8522e7170db542f32
Goodnews Atekha <goodnewsatekha@gmail.com>
2026-03-30 11:15:21 +0100

    Commit for consumers.py

---
+++ cyber_monitor/core/monitor/consumers.py
@@ -0,0 +1,25 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+
+class LogConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        user = self.scope["user"]
+
+        # Only allow authenticated users
+        if not user.is_authenticated:
+            await self.close()
+            return
+        
+        # Each company has its own group 
+
+        self.company_group_name = f"logs_company_{await self.get_company_id(user)}"
+
+        await self.channel_layer.group_add(self.company_group_name, self.channel_name)
+        await self.accept()
+
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(self.company_group_name, self.channel_name)
