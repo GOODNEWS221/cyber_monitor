46f0f9725fa48cb04d9d682306b26a474e4615d3
Goodnews Atekha <goodnewsatekha@gmail.com>
2026-03-30 09:59:56 +0100

    Commit for view.py

--- cyber_monitor/core/monitor/views.py
+++ cyber_monitor/core/monitor/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import NetworkLog, Alert
 # Create your views here.
+
+
+def dashboard(request):
+    company = request.user.userprofile.company
+
+    logs = NetworkLog.objects.filter(company=company).order_by('timestamp')[:50]
+    alerts = Alert.objects.filter(company=company).order_by('-timestamp')[:20]
+
+    return render( request, "dashboard.html", {
+        "logs": logs,
+        "alerts": alerts
+    })
+
